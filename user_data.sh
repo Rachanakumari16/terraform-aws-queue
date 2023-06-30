abba67a5f2d6ffc0a3fb8e7cee9e1b0ff55f03a2
Rachanakumari16 <138193969+Rachanakumari16@users.noreply.github.com>
2023-06-30 12:03:17 +0530

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -6,8 +6,8 @@ yum update -y
 #yum install rabbitmq-server -y
 wget https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.1/erlang-23.2.1-1.el7.x86_64.rpm
 yum localinstall erlang-23.2.1-1.el7.x86_64.rpm -y
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
-yum -y install rabbitmq-server
+wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.10.0/rabbitmq-server-3.10.0-1.el8.noarch.rpm	
+sudo rpm -Uvh rabbitmq-server-3.10.0-1.el8.noarch.rpm
 systemctl enable --now rabbitmq-server.service
 rabbitmq-plugins enable rabbitmq_management
 systemctl enable rabbitmq-server.service
@@ -16,6 +16,10 @@ systemctl stop rabbitmq-server.service
 truncate -s 0  /var/lib/rabbitmq/.erlang.cookie
 echo "XAIFUIBJAVHSEZOKOMHD" >>  /var/lib/rabbitmq/.erlang.cookie 
 systemctl start rabbitmq-server.service
+echo "------"
+echo "Env: ${environment_name}"
+echo "------"
+echo "Region: ${region}"
 export USERNAME="$(aws ssm get-parameter --name /${environment_name}/rabbit/USERNAME --with-decryption --output text --query Parameter.Value --region ${region})"
 echo "$USERNAME"
 export PASS="$(aws ssm get-parameter --name /${environment_name}/rabbit/PASSWORD --with-decryption --output text --query Parameter.Value --region ${region})"
